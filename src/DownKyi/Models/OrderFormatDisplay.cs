f4afb3890af20b39a4bb11e289c26ce42176bc30
小怪兽 <15234539022@163.com>
2023-11-09 14:22:35 +0800

    Update OrderFormatDisplay.cs
    xaml注释

--- src/DownKyi/Models/OrderFormatDisplay.cs
+++ src/DownKyi/Models/OrderFormatDisplay.cs
@@ -1,10 +1,21 @@
-﻿using DownKyi.Core.Settings;
+using DownKyi.Core.Settings;
 
 namespace DownKyi.Models
 {
+    /// <summary>
+    /// 表示订单格式显示的类
+    /// </summary>
     public class OrderFormatDisplay
     {
+        /// <summary>
+        /// 获取或设置名称
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// 获取或设置订单格式
+        /// </summary>
         public OrderFormat OrderFormat { get; set; }
     }
+
 }
