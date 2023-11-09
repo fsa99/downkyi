ca94570a4dac34e16a096f1807a3a56732cdd526
小怪兽 <15234539022@163.com>
2023-11-09 14:22:53 +0800

    Update ParseScopeDisplay.cs
    xaml注释

--- src/DownKyi/Models/ParseScopeDisplay.cs
+++ src/DownKyi/Models/ParseScopeDisplay.cs
@@ -1,10 +1,21 @@
-﻿using DownKyi.Core.Settings;
+using DownKyi.Core.Settings;
 
 namespace DownKyi.Models
 {
+    /// <summary>
+    /// 表示解析范围显示的类
+    /// </summary>
     public class ParseScopeDisplay
     {
+        /// <summary>
+        /// 获取或设置名称
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// 获取或设置解析范围
+        /// </summary>
         public ParseScope ParseScope { get; set; }
     }
+
 }
