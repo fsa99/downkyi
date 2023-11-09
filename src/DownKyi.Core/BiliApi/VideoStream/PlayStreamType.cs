85f3881976eedd5c141be0372c8d249cd46b803b
小怪兽 <15234539022@163.com>
2023-11-09 15:08:00 +0800

    Update PlayStreamType.cs
    xaml注释

--- src/DownKyi.Core/BiliApi/VideoStream/PlayStreamType.cs
+++ src/DownKyi.Core/BiliApi/VideoStream/PlayStreamType.cs
@@ -1,9 +1,18 @@
-﻿namespace DownKyi.Core.BiliApi.VideoStream
+namespace DownKyi.Core.BiliApi.VideoStream
 {
     public enum PlayStreamType
     {
-        VIDEO = 1, // 普通视频
-        BANGUMI,   // 番剧、电影、电视剧等
-        CHEESE,    // 课程
+        /// <summary>
+        /// 普通视频
+        /// </summary>
+        VIDEO = 1,
+        /// <summary>
+        /// 番剧、电影、电视剧等
+        /// </summary>
+        BANGUMI,
+        /// <summary>
+        /// 课程
+        /// </summary>
+        CHEESE,
     }
 }
