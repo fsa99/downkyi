d1f6c433af71c4115515aadd1fffe35642304101
小怪兽 <15234539022@163.com>
2023-11-09 14:59:39 +0800

    Update Dimension.cs
    xaml注释

--- src/DownKyi.Core/BiliApi/Models/Dimension.cs
+++ src/DownKyi.Core/BiliApi/Models/Dimension.cs
@@ -1,13 +1,24 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace DownKyi.Core.BiliApi.Models
 {
     public class Dimension : BaseModel
     {
+        /// <summary>
+        /// 宽度
+        /// </summary>
         [JsonProperty("width")]
         public int Width { get; set; }
+
+        /// <summary>
+        /// 高度
+        /// </summary>
         [JsonProperty("height")]
         public int Height { get; set; }
+
+        /// <summary>
+        /// 旋转角度
+        /// </summary>
         [JsonProperty("rotate")]
         public int Rotate { get; set; }
     }
