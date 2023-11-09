b872684888ae54175a23a7df92d52ac03e1f8228
小怪兽 <15234539022@163.com>
2023-11-09 14:22:03 +0800

    Update DownloadStatus.cs
    更改为xaml注释，方便代码阅读

--- src/DownKyi/Models/DownloadStatus.cs
+++ src/DownKyi/Models/DownloadStatus.cs
@@ -1,14 +1,41 @@
-﻿namespace DownKyi.Models
+namespace DownKyi.Models
 {
     public enum DownloadStatus
     {
-        NOT_STARTED,       // 未开始，从未开始下载
-        WAIT_FOR_DOWNLOAD, // 等待下载，下载过，但是启动本次下载周期未开始，如重启程序后未开始
-        PAUSE_STARTED,     // 暂停启动下载
-        PAUSE,             // 暂停
+        /// <summary>
+        /// 未开始，从未开始下载
+        /// </summary>
+        NOT_STARTED,
+        /// <summary>
+        /// 等待下载，下载过，但是启动本次下载周期未开始，如重启程序后未开始
+        /// </summary>
+        WAIT_FOR_DOWNLOAD,
+
+        /// <summary>
+        /// 暂停启动下载
+        /// </summary>
+        PAUSE_STARTED,
+
+        /// <summary>
+        /// 暂停
+        /// </summary>
+        PAUSE,
+
+        /// <summary>
+        /// 下载中
+        /// </summary>
+        DOWNLOADING,
+
+        /// <summary>
+        /// 下载成功
+        /// </summary>
+        DOWNLOAD_SUCCEED,
+
+        /// <summary>
+        /// 下载失败
+        /// </summary>
+        DOWNLOAD_FAILED,
+
         //PAUSE_TO_WAIT,     // 暂停后等待
-        DOWNLOADING,       // 下载中
-        DOWNLOAD_SUCCEED,  // 下载成功
-        DOWNLOAD_FAILED,   // 下载失败
     }
 }
