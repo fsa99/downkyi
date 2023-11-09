2bff563bf5f1099e35d6a6035641ed02d0e37289
小怪兽 <15234539022@163.com>
2023-11-09 15:01:07 +0800

    Update VideoOwner.cs
    xaml注释

--- src/DownKyi.Core/BiliApi/Models/VideoOwner.cs
+++ src/DownKyi.Core/BiliApi/Models/VideoOwner.cs
@@ -1,13 +1,19 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace DownKyi.Core.BiliApi.Models
 {
+    /// <summary>
+    /// 视频的作者
+    /// </summary>
     public class VideoOwner : BaseModel
     {
         [JsonProperty("mid")]
         public long Mid { get; set; }
         [JsonProperty("name")]
         public string Name { get; set; }
+        /// <summary>
+        /// 用户头像
+        /// </summary>
         [JsonProperty("face")]
         public string Face { get; set; }
     }
