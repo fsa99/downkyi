04331fcd00954a31817fa0e82b03292354725205
小怪兽 <15234539022@163.com>
2023-11-14 14:11:40 +0800

    Update AfterDownloadOperation.cs
    新增 创建upup资源的操作

--- src/DownKyi.Core/Settings/AfterDownloadOperation.cs
+++ src/DownKyi.Core/Settings/AfterDownloadOperation.cs
@@ -1,11 +1,33 @@
-﻿namespace DownKyi.Core.Settings
+namespace DownKyi.Core.Settings
 {
+    /// <summary>
+    /// 下载完成后的操作 枚举
+    /// </summary>
     public enum AfterDownloadOperation
     {
+        /// <summary>
+        /// 没有设置
+        /// </summary>
         NOT_SET = 0,
+        /// <summary>
+        /// 什么都不做
+        /// </summary>
         NONE = 1,
+        /// <summary>
+        /// 打开文件夹
+        /// </summary>
         OPEN_FOLDER,
+        /// <summary>
+        /// 关闭APP
+        /// </summary>
         CLOSE_APP,
-        CLOSE_SYSTEM
+        /// <summary>
+        /// 关闭系统
+        /// </summary>
+        CLOSE_SYSTEM,
+        /// <summary>
+        /// 创建UpUp壁纸资源
+        /// </summary>
+        CREATE_UPUPRESOURCE
     }
 }
