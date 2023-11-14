6ee045c86a78a7edd9db20219f31ce4eb71c7d5a
小怪兽 <15234539022@163.com>
2023-11-14 14:09:23 +0800

    Update DownloadFinishedSort.cs
    xaml注释

--- src/DownKyi.Core/Settings/DownloadFinishedSort.cs
+++ src/DownKyi.Core/Settings/DownloadFinishedSort.cs
@@ -1,9 +1,21 @@
-﻿namespace DownKyi.Core.Settings
+namespace DownKyi.Core.Settings
 {
+    /// <summary>
+    /// 下载完成后排序
+    /// </summary>
     public enum DownloadFinishedSort
     {
+        /// <summary>
+        /// 无设置
+        /// </summary>
         NOT_SET = 0,
+        /// <summary>
+        /// 下载顺序
+        /// </summary>
         DOWNLOAD,
+        /// <summary>
+        /// 数字
+        /// </summary>
         NUMBER
     }
 }
