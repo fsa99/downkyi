c66a409b2e10e4edd09ac6808629b46e52e70e84
小怪兽 <15234539022@163.com>
2023-11-14 14:10:41 +0800

    Update ParseScope.cs
    xaml注释

--- src/DownKyi.Core/Settings/ParseScope.cs
+++ src/DownKyi.Core/Settings/ParseScope.cs
@@ -1,11 +1,23 @@
-﻿namespace DownKyi.Core.Settings
+namespace DownKyi.Core.Settings
 {
+    /// <summary>
+    /// 视频解析范围
+    /// </summary>
     public enum ParseScope
     {
         NOT_SET = 0,
         NONE = 1,
+        /// <summary>
+        /// 选定的项目
+        /// </summary>
         SELECTED_ITEM,
+        /// <summary>
+        /// 当前部分
+        /// </summary>
         CURRENT_SECTION,
+        /// <summary>
+        /// 所有
+        /// </summary>
         ALL
     }
 }
