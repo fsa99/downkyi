4f9ce72319ea794406bdc1ad952e4101bc9c573e
小怪兽 <15234539022@163.com>
2023-11-18 22:28:10 +0800

    错误的cs文件修正
    错误的cs文件修正

--- src/DownKyi.Core/BiliApi/Favorites/Models/MediaStatus.cs
+++ src/DownKyi.Core/BiliApi/Favorites/Models/MediaStatus.cs
@@ -3,33 +3,13 @@ using Newtonsoft.Json;
 
 namespace DownKyi.Core.BiliApi.Favorites.Models
 {
-    /// <summary>
-    /// 视频被喜欢的状态
-    /// </summary>
-    public class FavStatus : BaseModel
+    public class MediaStatus : BaseModel
     {
-        /// <summary>
-        /// 收藏数
-        /// </summary>
         [JsonProperty("collect")]
         public long Collect { get; set; }
-
-        /// <summary>
-        /// 播放数
-        /// </summary>
         [JsonProperty("play")]
         public long Play { get; set; }
-
-        /// <summary>
-        /// 点赞数
-        /// </summary>
-        [JsonProperty("thumb_up")]
-        public long ThumbUp { get; set; }
-
-        /// <summary>
-        /// 分享数
-        /// </summary>
-        [JsonProperty("share")]
-        public long Share { get; set; }
+        [JsonProperty("danmaku")]
+        public long Danmaku { get; set; }
     }
 }
