623104595c9edb42b40a54de1ae3a223e99e2b3d
小怪兽 <15234539022@163.com>
2023-11-23 20:15:03 +0800

    Update Downloader.cs

--- src/DownKyi.Core/Settings/Downloader.cs
+++ src/DownKyi.Core/Settings/Downloader.cs
@@ -1,10 +1,25 @@
-﻿namespace DownKyi.Core.Settings
+namespace DownKyi.Core.Settings
 {
+    /// <summary>
+    /// 下载器
+    /// </summary>
     public enum Downloader
     {
+        /// <summary>
+        /// 没有设置
+        /// </summary>
         NOT_SET = 0,
+        /// <summary>
+        /// 内置下载器
+        /// </summary>
         BUILT_IN,
+        /// <summary>
+        /// Aria2下载器
+        /// </summary>
         ARIA,
+        /// <summary>
+        /// 自定义Aria下载器
+        /// </summary>
         CUSTOM_ARIA,
     }
 }
