e3af6cdde0e9a29c57a273a7a3aa20e89aa7d819
小怪兽 <15234539022@163.com>
2023-12-12 11:34:17 +0800

    Update DownloadFinishedSort.cs

--- src/DownKyi.Core/Settings/DownloadFinishedSort.cs
+++ src/DownKyi.Core/Settings/DownloadFinishedSort.cs
@@ -10,12 +10,28 @@ namespace DownKyi.Core.Settings
         /// </summary>
         NOT_SET = 0,
         /// <summary>
-        /// 下载顺序
+        /// 下载完成时间 排序
         /// </summary>
         DOWNLOAD,
         /// <summary>
-        /// 数字
+        /// UP主ID 排序
         /// </summary>
-        NUMBER
+        UPZHUID,
+        /// <summary>
+        /// 按Order 排序
+        /// </summary>
+        NUMBER,
+        /// <summary>
+        /// 文件大小 排序
+        /// </summary>
+        FILESIZE,
+        /// <summary>
+        /// 视屏时长 排序
+        /// </summary>
+        VIDEODURATION,
+        /// <summary>
+        /// 分区ID 排序
+        /// </summary>
+        ZONEID,
     }
 }
