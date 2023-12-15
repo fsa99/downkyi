5e00586c38cbdd2b256af4a82fb69b975873bbfb
小怪兽 <15234539022@163.com>
2023-12-15 18:55:54 +0800

    Update ClipboardHooker.cs

--- src/DownKyi/Utils/ClipboardHooker.cs
+++ src/DownKyi/Utils/ClipboardHooker.cs
@@ -1,24 +1,37 @@
-﻿using System;
+using System;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
 
 namespace DownKyi.Utils
 {
+    /// <summary>
+    /// 剪贴板监听器类，用于在 Windows 操作系统中监听剪贴板的更新事件。
+    /// </summary>
     public class ClipboardHooker : IDisposable
     {
+        // 剪贴板更新消息的常量值
         private const int WM_CLIPBOARDUPDATE = 0x031D;
 
+        // 外部函数，向系统注册剪贴板格式监听器
         [DllImport("User32.dll")]
         private static extern bool AddClipboardFormatListener(IntPtr hwnd);
 
+        // 与窗口句柄关联的 HwndSource 实例
         private readonly HwndSource _hwndSource = null;
 
+        /// <summary>
+        /// 析构函数，释放资源
+        /// </summary>
         public void Dispose()
         {
             _hwndSource?.Dispose();
         }
 
+        /// <summary>
+        /// 构造函数，初始化剪贴板监听器
+        /// </summary>
+        /// <param name="window">要监听剪贴板事件的窗口对象</param>
         public ClipboardHooker(Window window)
         {
             WindowInteropHelper helper = new WindowInteropHelper(window);
@@ -30,8 +43,12 @@ namespace DownKyi.Utils
             }
         }
 
+        /// <summary>
+        /// 剪贴板更新事件的消息处理方法
+        /// </summary>
         private IntPtr OnHooked(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
+            // 当剪贴板更新消息发生时，触发 ClipboardUpdated 事件
             if (msg == WM_CLIPBOARDUPDATE)
             {
                 ClipboardUpdated?.Invoke(this, EventArgs.Empty);
@@ -40,6 +57,10 @@ namespace DownKyi.Utils
             return IntPtr.Zero;
         }
 
+        /// <summary>
+        /// 剪贴板更新事件
+        /// </summary>
         public event EventHandler ClipboardUpdated;
     }
 }
+
