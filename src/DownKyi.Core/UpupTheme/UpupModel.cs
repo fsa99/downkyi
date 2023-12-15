a22e0c6f6542425d2a87476283ced3f18c3cea4f
小怪兽 <15234539022@163.com>
2023-12-15 18:28:26 +0800

    Create UpupModel.cs

---
+++ src/DownKyi.Core/UpupTheme/UpupModel.cs
@@ -0,0 +1,105 @@
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace DownKyi.Core.UpupTheme
+{
+    [Serializable]
+    public class UpupModel
+    {
+        /// <summary>
+        /// 使用者 名字
+        /// </summary>
+        [JsonProperty("UserName")]
+        public string UserName { get; set; }
+        /// <summary>
+        /// 描述
+        /// </summary>
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        /// <summary>
+        /// 是否原创 
+        /// </summary>
+        [JsonProperty("isOriginal")]
+        public int IsOriginal { get; set; }
+        private string reprintUrl;
+        /// <summary>
+        /// 视频原地址
+        /// </summary>
+        [JsonProperty("reprintUrl")]
+        public string ReprintUrl
+        {
+            get => reprintUrl;
+            set => reprintUrl = Path.Combine("https://www.bilibili.com/video/", value.ToString());
+        }
+        /// <summary>
+        /// 作者
+        /// </summary>
+        [JsonProperty("author")]
+        public string Author { get; set; }
+        /// <summary>
+        /// 作品名
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        /// <summary>
+        /// 视屏本地地址
+        /// </summary>
+        [JsonProperty("src")]
+        public string Src { get; set; }
+        /// <summary>
+        /// 标记
+        /// </summary>
+        [JsonProperty("tag")]
+        public string Tag { get; set; }
+        /// <summary>
+        /// 用户界面类型
+        /// </summary>
+        [JsonProperty("themeType")]
+        public int ThemeType { get; set; }
+
+        private int themeno;
+        /// <summary>
+        /// 主题编号 唯一
+        /// </summary>
+        [JsonProperty("themeno")]
+        public int Themeno
+        {
+            get => themeno;
+            set
+            {
+                Url = value.ToString() + "/theme.upup";
+                themeno = value;
+            }
+        }
+        private string url;
+        /// <summary>
+        /// 配置文件相对网络路径
+        /// </summary>
+        [JsonProperty("url")]
+        public string Url
+        {
+            get => url;
+            set => url = "http://source.upupoo.com/theme/" + value;
+        }
+        /// <summary>
+        /// 已使用
+        /// </summary>
+        [JsonProperty("used")]
+        public int Used { get; set; }
+        /// <summary>
+        /// 版本
+        /// </summary>
+        [JsonProperty("ver")]
+        public string Ver { get; set; }
+
+        public UpupModel()
+        {
+            ThemeType = 1;
+            Used = 1;
+            IsOriginal = 1;
+            Ver = "1.0";
+        }
+    }
+}
+
