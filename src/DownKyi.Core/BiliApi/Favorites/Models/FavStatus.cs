58ffc3207bc7cd44afbb9733b68d2da369526025
小怪兽 <15234539022@163.com>
2023-12-15 18:40:58 +0800

    Update FavStatus.cs

--- src/DownKyi.Core/BiliApi/Favorites/Models/FavStatus.cs
+++ src/DownKyi.Core/BiliApi/Favorites/Models/FavStatus.cs
@@ -1,16 +1,34 @@
-﻿using DownKyi.Core.BiliApi.Models;
+using DownKyi.Core.BiliApi.Models;
 using Newtonsoft.Json;
 
 namespace DownKyi.Core.BiliApi.Favorites.Models
 {
+    /// <summary>
+    /// 视频被喜欢的状态
+    /// </summary>
     public class FavStatus : BaseModel
     {
+        /// <summary>
+        /// 收藏数
+        /// </summary>
         [JsonProperty("collect")]
         public long Collect { get; set; }
+
+        /// <summary>
+        /// 播放数
+        /// </summary>
         [JsonProperty("play")]
         public long Play { get; set; }
+
+        /// <summary>
+        /// 点赞数
+        /// </summary>
         [JsonProperty("thumb_up")]
         public long ThumbUp { get; set; }
+
+        /// <summary>
+        /// 分享数
+        /// </summary>
         [JsonProperty("share")]
         public long Share { get; set; }
     }
