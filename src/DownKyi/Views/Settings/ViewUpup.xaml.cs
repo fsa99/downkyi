89de2d27fd3dcbe812cf27b0747d7dd8401892ec
小怪兽 <15234539022@163.com>
2023-12-15 19:21:20 +0800

    Create ViewUpup.xaml.cs

---
+++ src/DownKyi/Views/Settings/ViewUpup.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace DownKyi.Views.Settings
+{
+    /// <summary>
+    /// Interaction logic for ViewUpup
+    /// </summary>
+    public partial class ViewUpup : UserControl
+    {
+        public ViewUpup()
+        {
+            InitializeComponent();
+        }
+    }
+}
