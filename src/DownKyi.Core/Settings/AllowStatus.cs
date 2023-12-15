aff3597a4459fd3c2d61a3f338f5ee64d323a269
小怪兽 <15234539022@163.com>
2023-12-15 18:33:51 +0800

    Update AllowStatus.cs

--- src/DownKyi.Core/Settings/AllowStatus.cs
+++ src/DownKyi.Core/Settings/AllowStatus.cs
@@ -1,9 +1,18 @@
-﻿namespace DownKyi.Core.Settings
+namespace DownKyi.Core.Settings
 {
     public enum AllowStatus
     {
+        /// <summary>
+        /// 没设置
+        /// </summary>
         NONE = 0,
+        /// <summary>
+        /// 否
+        /// </summary>
         NO,
+        /// <summary>
+        /// 是
+        /// </summary>
         YES
     }
 }
