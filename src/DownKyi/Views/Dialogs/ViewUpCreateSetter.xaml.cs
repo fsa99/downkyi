d80d613b27d4dd1c7f8db3a86ddc2c63616b120d
小怪兽 <15234539022@163.com>
2023-12-15 19:17:46 +0800

    Create ViewUpCreateSetter.xaml.cs

---
+++ src/DownKyi/Views/Dialogs/ViewUpCreateSetter.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace DownKyi.Views.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for ViewDownloadSetter
+    /// </summary>
+    public partial class ViewUpCreateSetter : UserControl
+    {
+        public ViewUpCreateSetter()
+        {
+            InitializeComponent();
+        }
+    }
+}
