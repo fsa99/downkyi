385309fe36da53bd55ad3912290a0b29989d0dfe
小怪兽 <15234539022@163.com>
2023-12-15 18:58:42 +0800

    Update DownloadedItem.cs

--- src/DownKyi/ViewModels/DownloadManager/DownloadedItem.cs
+++ src/DownKyi/ViewModels/DownloadManager/DownloadedItem.cs
@@ -1,9 +1,16 @@
+using DownKyi.Core.Settings;
+using DownKyi.Core.Storage;
+using DownKyi.Core.UpupTheme;
 using DownKyi.Images;
 using DownKyi.Models;
 using DownKyi.Services;
 using DownKyi.Utils;
+using DownKyi.ViewModels.Dialogs;
+using Newtonsoft.Json;
 using Prism.Commands;
 using Prism.Services.Dialogs;
+using System;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 
@@ -14,6 +21,8 @@ namespace DownKyi.ViewModels.DownloadManager
         public DownloadedItem() : this(null)
         {
         }
+
+        public event Action<string> PublishTip;
 
         public DownloadedItem(IDialogService dialogService) : base(dialogService)
         {
@@ -176,7 +185,154 @@ namespace DownKyi.ViewModels.DownloadManager
             App.DownloadedList.Remove(this);
         }
 
-        #endregion
+        // 生成UPUPoo资源事件
+        private DelegateCommand createUpupTheme;
+        public DelegateCommand CreateUpupTheme => createUpupTheme ?? (createUpupTheme = new DelegateCommand(ExecuteCreateUpupThemeCommand));
 
+        private void ExecuteCreateUpupThemeCommand()
+        {
+            if (DownloadBase == null) { return; }
+            // 选择文件夹
+            string directory = SetDirectory(DialogService);
+            if (string.IsNullOrEmpty(directory))
+            {
+
+            }
+            UpupModel upupModel = new UpupModel()
+            {
+                UserName = SettingsManager.GetInstance().GetDefaultUseName(),
+                Author = DownloadBase.UpOwner.Name,
+                Name = DownloadBase.Name,
+                Src = DownloadBase.FilePath + ".mp4",
+                Themeno = (int)DownloadBase.Avid,
+                Tag = DownloadBase.ZoneId.ToString(),
+                Description = DownloadBase.Description,
+                ReprintUrl = DownloadBase.Bvid,
+            };
+            string targetDirectory = Path.Combine(directory, upupModel.Themeno.ToString());
+            if (!Directory.Exists(targetDirectory))
+            {
+                Directory.CreateDirectory(targetDirectory);
+            }
+            string sourceIMGFileName = DownloadBase.FilePath + ".jpg";
+            string targetIMGFileName = Path.Combine(targetDirectory, "preview.jpg");
+            string sourceVideoFileName = DownloadBase.FilePath + ".mp4";
+            string targetVideoFileName = Path.Combine(targetDirectory, Path.GetFileName(sourceVideoFileName));
+            // 判断是否下载了视频
+            if (DownloadBase.NeedDownloadContent.ContainsKey("downloadVideo") && DownloadBase.NeedDownloadContent["downloadVideo"])
+            {
+                if (!File.Exists(sourceVideoFileName))
+                {
+                    // 视频文件不存在或被删除
+                    return;
+                }
+                //判断视频是复制到目标目录  还是放在源  Src要做对应改变
+                if (SettingsManager.GetInstance().GetIsMoveVideoUpDirectory() == AllowStatus.YES)
+                {
+                    System.Threading.Tasks.Task.Run(() => File.Copy(sourceVideoFileName, targetVideoFileName, true));
+                    upupModel.Src = targetVideoFileName;
+                }
+                // 判断有没有下载封面
+                #region 封面的处理
+                if (DownloadBase.NeedDownloadContent.ContainsKey("downloadCover") && DownloadBase.NeedDownloadContent["downloadCover"])
+                {
+                    
+                    if (File.Exists(sourceIMGFileName))
+                    {
+                        File.Move(sourceIMGFileName, targetIMGFileName);
+                    }
+                    else
+                    {
+                        // 查询、保存封面
+                        StorageCover storageCover = new StorageCover();
+                        string cover = storageCover.GetCover(DownloadBase.Avid, DownloadBase.Bvid, DownloadBase.Cid, DownloadBase.CoverUrl);
+                        if (cover == null)
+                        {
+                            // 本地不存在且下载失败
+                        }
+                        using (Bitmap bitmap = new Bitmap(cover))
+                        {
+                            bitmap.Save(targetIMGFileName);
+                        }
+                    }
+                }
+                else
+                {
+                    // 查询、保存封面
+                    StorageCover storageCover = new StorageCover();
+                    string cover = storageCover.GetCover(DownloadBase.Avid, DownloadBase.Bvid, DownloadBase.Cid, DownloadBase.CoverUrl);
+                    if (cover == null)
+                    {
+                        // 本地不存在且下载失败
+                    }
+                    using (Bitmap bitmap = new Bitmap(cover))
+                    {
+                        bitmap.Save(targetIMGFileName);
+                    }
+                }
+                #endregion
+                string themeupup = JsonConvert.SerializeObject(upupModel, Formatting.Indented);
+                File.WriteAllText(Path.Combine(targetDirectory, "theme.upup"), themeupup);
+                PublishTip?.Invoke("成功生成了upup资源");
+
+            }
+            else
+            {
+                // 没有下载视频
+                PublishTip?.Invoke("该完成项没有下载视频");
+            }
+        }
+        #endregion
+        /// <summary>
+        /// 选择文件夹
+        /// </summary>
+        /// <param name="dialogService"></param>
+        public string SetDirectory(IDialogService dialogService)
+        {
+            // 选择的下载文件夹
+            string directory = string.Empty;
+
+            // 是否使用默认下载目录
+            if (SettingsManager.GetInstance().GetIsUseSaveUpupRootPath() == AllowStatus.YES)
+            {
+                directory = SettingsManager.GetInstance().GetSaveUpupRootPath();
+            }
+            else
+            {
+                // 打开文件夹选择器
+                dialogService.ShowDialog(ViewUpCreateSetterViewModel.Tag, null, result =>
+                {
+                    if (result.Result == ButtonResult.OK)
+                    {
+                        // 选择的下载文件夹
+                        directory = result.Parameters.GetValue<string>("directory");
+                    }
+                });
+            }
+            if (string.IsNullOrEmpty(directory))
+            {
+                return null;
+            }
+            if (!Directory.Exists(Directory.GetDirectoryRoot(directory)))
+            {
+                var alert = new AlertService(dialogService);
+                alert.ShowError(DictionaryResource.GetString("DriveNotFound"));
+
+                directory = string.Empty;
+            }
+
+            // 下载设置dialog中如果点击取消或者关闭窗口，
+            // 会返回空字符串，
+            // 这时直接退出
+            if (directory == null || directory == string.Empty) { return null; }
+
+            // 文件夹不存在则创建
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            return directory;
+        }
     }
 }
