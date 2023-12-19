69b492133b4b46b08c67433fb96b45d4671be361
小怪兽 <15234539022@163.com>
2023-12-19 20:05:19 +0800

    Update UpupWallpaper.cs

--- src/DownKyi.Core/UpupTheme/UpupWallpaper.cs
+++ src/DownKyi.Core/UpupTheme/UpupWallpaper.cs
@@ -1,28 +1,62 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace DownKyi.Core.UpupTheme
 {
     public class UpupWallpaper
     {
+        private static readonly string defaultFileName = "upWallpaper.json";
+        [JsonIgnore]
+        private string JsonFilePath { get; set; }
+
         private List<UpupModel> _upupData;
         [JsonProperty("data")]
-        public List<UpupModel> UpupData 
+        public List<UpupModel> UpupData
         {
             get => _upupData;
-            set{
-                _upupData = value;
-                UpdateTotal();
-            }
+            set => _upupData = value;
         }
 
         [JsonProperty("total")]
-        public int Total { get; private set; }
-
-        private void UpdateTotal()
+        private int Total 
         {
-            Total = _upupData.Count;
+            get => _upupData.Count; 
+        }
+
+        private void UpupDataAdd(UpupModel upup)
+        {
+            if (!UpupData.Any(item => item.Themeno == upup.Themeno))
+            {
+                UpupData.Add(upup);
+                WriteJsonData();
+            }
+        }
+
+        public UpupWallpaper(string directory, UpupModel upup)
+        {
+            string filePath = Path.Combine(directory, defaultFileName);
+            JsonFilePath = filePath;
+            if (File.Exists(filePath))
+            {
+                string jsonContent = File.ReadAllText(filePath);
+                JsonConvert.PopulateObject(jsonContent, this);
+            }
+            if (UpupData == null)
+            {
+                UpupData = new List<UpupModel>();
+            }
+            UpupDataAdd(upup);
+        }
+        /// <summary>
+        /// 写成json文件
+        /// </summary>
+        private void WriteJsonData()
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(JsonFilePath));
+            File.WriteAllText(JsonFilePath, JsonConvert.SerializeObject(this, Formatting.Indented));
         }
     }
 }
