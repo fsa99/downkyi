e9f2f2686206f3af471e4081dd266fbf711e55e5
小怪兽 <15234539022@163.com>
2023-12-28 14:55:22 +0800

    Create CustomSimplePager.xaml.cs

---
+++ src/DownKyi/CustomControl/CustomSimplePager.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace DownKyi.CustomControl
+{
+    /// <summary>
+    /// CustomPager.xaml 的交互逻辑
+    /// </summary>
+    public partial class CustomSimplePager : UserControl
+    {
+        public CustomSimplePager()
+        {
+            InitializeComponent();
+        }
+    }
+}
