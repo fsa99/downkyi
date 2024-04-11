bd94210c8a971cb11970cdb7ea5f73778fe115d3
小怪兽 <15234539022@163.com>
2024-04-11 15:18:37 +0800

    Update Downloaded.cs

--- src/DownKyi/Models/Downloaded.cs
+++ src/DownKyi/Models/Downloaded.cs
@@ -9,6 +9,27 @@ namespace DownKyi.Models
         {
         }
 
+        /// <summary>
+        /// 静态方法，将 dynamic 转化为 Downloaded 类型
+        /// </summary>
+        /// <param name="dynamicObj"></param>
+        /// <returns></returns>
+        public static Downloaded FromDynamic(dynamic dynamicObj)
+        {
+            Downloaded downloaded = new Downloaded
+            {
+                MaxSpeedDisplay = dynamicObj.MaxSpeedDisplay as string,
+                FinishedTimestamp = dynamicObj.FinishedTimestamp as long? ?? 0 // 如果为null，默认为0
+            };
+            // 如果 FinishedTimestamp 为0，则不设置 FinishedTime
+            if (downloaded.FinishedTimestamp != 0)
+            {
+                downloaded.SetFinishedTimestamp(downloaded.FinishedTimestamp);
+            }
+
+            return downloaded;
+        }
+
         //  下载速度
         public string MaxSpeedDisplay { get; set; }
 
